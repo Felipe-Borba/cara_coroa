d30cc76ca6cd00b1c8169aad97f03ce3a86ab91c
Felipe Borba <felipe.borba0109@gmail.com>
2024-10-08 23:58:24 -0300

    add pages

---
+++ lib/Jogar.dart
@@ -0,0 +1,42 @@
+import 'package:cara_coroa/Resultado.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Jogar extends StatefulWidget {
+  const Jogar({super.key});
+
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  void _exibirResultado() {
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Resultado(resultado)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff61bd86),
+      //backgroundColor: Color.fromRGBO(255, 204, 128, 1),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Image.asset("assets/images/logo.png"),
+          GestureDetector(
+            onTap: _exibirResultado,
+            child: Image.asset("assets/images/botao_jogar.png"),
+          )
+        ],
+      ),
+    );
+  }
+}
